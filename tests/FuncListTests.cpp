1b0b992fdadedfef3ef03ef88b3c082a80143bb3
krove44 <krove8@gmail.com>
2026-05-11 00:54:57 +0300

    add tests for FuncList

---
+++ tests/FuncListTests.cpp
@@ -0,0 +1,100 @@
+#include <gtest/gtest.h>
+#include "../Structur/FuncList.h"
+
+TEST(FuncList, DefaultConstructor) {
+    FuncList<int> list;
+    EXPECT_EQ(list.size(), 0);
+    EXPECT_TRUE(list.empty());
+}
+
+TEST(FuncList, Prepend) {
+    FuncList<int> list;
+    list = list.prepend(1);
+    EXPECT_EQ(list.size(), 1);
+    EXPECT_EQ(list.head(), 1);
+}
+
+TEST(FuncList, PrependMultiple) {
+    FuncList<int> list;
+    list = list.prepend(3).prepend(2).prepend(1);
+    EXPECT_EQ(list.size(), 3);
+    EXPECT_EQ(list.head(), 1);
+    EXPECT_EQ(list.tail().head(), 2);
+    EXPECT_EQ(list.tail().tail().head(), 3);
+}
+
+TEST(FuncList, PrependDoesNotMutate) {
+    FuncList<int> a;
+    a = a.prepend(1);
+    FuncList<int> b = a.prepend(0);
+    EXPECT_EQ(a.size(), 1);
+    EXPECT_EQ(a.head(), 1);
+    EXPECT_EQ(b.size(), 2);
+    EXPECT_EQ(b.head(), 0);
+}
+
+TEST(FuncList, HeadEmpty) {
+    FuncList<int> list;
+    EXPECT_THROW(list.head(), FuncListEmpthy);
+}
+
+TEST(FuncList, TailEmpty) {
+    FuncList<int> list;
+    EXPECT_THROW(list.tail(), FuncListEmpthy);
+}
+
+TEST(FuncList, Tail) {
+    FuncList<int> list;
+    list = list.prepend(2).prepend(1);
+    FuncList<int> t = list.tail();
+    EXPECT_EQ(t.size(), 1);
+    EXPECT_EQ(t.head(), 2);
+}
+
+TEST(FuncList, Concat) {
+    FuncList<int> a;
+    a = a.prepend(2).prepend(1);
+    FuncList<int> b;
+    b = b.prepend(4).prepend(3);
+    FuncList<int> result = a.concat(b);
+    EXPECT_EQ(result.size(), 4);
+    EXPECT_EQ(result.head(), 1);
+    EXPECT_EQ(result.tail().head(), 2);
+    EXPECT_EQ(result.tail().tail().head(), 3);
+    EXPECT_EQ(result.tail().tail().tail().head(), 4);
+}
+
+TEST(FuncList, ConcatEmpty) {
+    FuncList<int> a;
+    a = a.prepend(1);
+    FuncList<int> b;
+    FuncList<int> result = a.concat(b);
+    EXPECT_EQ(result.size(), 1);
+    EXPECT_EQ(result.head(), 1);
+}
+
+TEST(FuncList, ConcatWithEmpty) {
+    FuncList<int> a;
+    FuncList<int> b;
+    b = b.prepend(1);
+    FuncList<int> result = a.concat(b);
+    EXPECT_EQ(result.size(), 1);
+    EXPECT_EQ(result.head(), 1);
+}
+
+TEST(FuncList, ConcatDoesNotMutate) {
+    FuncList<int> a;
+    a = a.prepend(1);
+    FuncList<int> b;
+    b = b.prepend(2);
+    a.concat(b);
+    EXPECT_EQ(a.size(), 1);
+    EXPECT_EQ(b.size(), 1);
+}
+
+TEST(FuncList, Double) {
+    FuncList<double> list;
+    list = list.prepend(2.5).prepend(1.5);
+    EXPECT_DOUBLE_EQ(list.head(), 1.5);
+    EXPECT_DOUBLE_EQ(list.tail().head(), 2.5);
+}
