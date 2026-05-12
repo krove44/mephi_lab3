2171380443edd2940414af48c62cdd0b5b4e80de
krove44 <krove8@gmail.com>
2026-05-12 19:55:22 +0300

    it's work :)

--- Structur/Vector.h
+++ Structur/Vector.h
@@ -1,211 +1,101 @@
 #pragma once
-#include "../submodule/DynamicArray.h"
-#include <cmath>
 #include <cstddef>
+#include <concepts>
 #include <utility>
 #include "../Exception/VectorException.h"
-#include "span"
-#include <iostream>
-
 
 template<typename T>
-class Vector {
-private:
-    DynamicArray<T> sp_;
-    size_t size_;
+concept number = requires (T t) {
+    {t + t} -> std::same_as<T>;
+    {t - t} -> std::same_as<T>;
+    {t * t} -> std::same_as<T>;
+};
 
-class Iterator {
-        T* cur;
-        friend class Vector<T>;
-    public:
-        explicit Iterator(T* ptr) : cur(ptr) {};
-
-        Iterator operator+(int n) const {
-            Iterator copy(cur + n);
-            return copy;
-            } 
-            
-        Iterator operator-(int n) const {
-            Iterator copy(cur - n);
-            return copy;
-            }
-        
-        Iterator& operator++() {
-            ++cur;
-            return *this; 
-            } 
-
-        Iterator& operator--() {
-            --cur;
-            return *this;
-            } 
-
-        Iterator operator++(int) {
-            Iterator old = *this;
-            cur++;
-            return old;
-            } 
-
-        Iterator operator--(int) {
-            Iterator old = *this;
-            cur--;
-            return old;
-            }
-
-        bool operator==(const Iterator& other) const {
-            return cur == other.cur;
-            };
-
-        bool operator!=(const Iterator& other) const {
-            return cur != other.cur;
-            };
-
-        T& operator*() const {
-            return *cur;
-        }
-    };
-
-public:
-    Iterator begin() {
-        return Iterator(&sp_[0]);
-    }
-    Iterator end() {
-        return Iterator(&sp_[0] + size_);
-    }
-
-    const Iterator begin() const {
-        return Iterator(const_cast<T*>(&sp_[0]));
-    }
-    const Iterator end() const {
-        return Iterator(const_cast<T*>(&sp_[0] + size_));
-    }
-
-    Vector() : sp_{}, size_{}{};
-    Vector(size_t size) : sp_(size), size_(size){};
-    Vector(std::span<const T> data) : sp_(data.size()), size_(data.size()) {
-        size_t j = 0;
-        for (const T& i : data) {
-            sp_[j++] = i;
-        }
-    };
-    Vector(const Vector<T>& other) : sp_(other.sp_), size_(other.size_){};
-    Vector(Vector<T>&& other) : sp_(std::move(other.sp_)), size_(std::move(other.size_)){}; 
-
-    size_t size() const {
-        return size_;
-    }
-    
-    Vector<T>* push_back(T item) {
-        if (size_ == sp_.GetSize()){
-            sp_.Resize(size_ == 0 ? 1 : 2 * size_);
-        }
-        sp_[size_] = item;
-        size_++;
-        return this;
-    }
-
-    double norm() {
-        double ans{};
-        for(T& i : *this) {
-            ans += std::abs(i)*std::abs(i);
-        }
-        return sqrt(ans);
-    }
-
-    T dot(const Vector<T>& other){
-        if (size_ == other.size_){
-            T ans{};
-            for (size_t i = 0; i < size_; i++) {
-                ans += sp_[i]*other[i];
-            }
-            return ans;
-        }
-        throw SizeMismatchException(size_, other.size_);
-    };
-
-    T& operator[](size_t index) {
-        return sp_[index];
-    }
-    
-    const T& operator[](size_t index) const {
-        return sp_[index];
-    }
-
-    bool operator==(const Vector<T>& other) const {
-        if (this == &other) {
-            return true;
-        }
-        else if (size_ == other.size_){
-            for (size_t i = 0; i < size_; i++) {
-                if(sp_[i] != other[i]){
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-
-    Vector<T> operator+(const Vector<T>& other) {
-        if (size_ == other.size_){
-            Vector<T> new_vector(*this);
-            size_t j = 0;
-            for(T& i: new_vector) {
-                i += other[j++];
-            }
-            return new_vector;
-        }
-        throw SizeMismatchException(size_, other.size_);
-    };
-
-    Vector<T> operator*(T scalar) {
-        Vector<T> new_vector(*this);
-        for(T& i : new_vector) {
-            i *= scalar;
-        }
-        return new_vector;
-    };
-
-    template<typename F>
-    Vector<T> map(F func) const {
-        size_t j = 0;
-        Vector<T> res(size_);
-        for (T& i : *this) {
-            res[j++] = func(i);
-        }
-        return res;
-    };
-
-    template<typename F>
-    Vector<T> where(F func) const {
-        Vector<T> res;
-        size_t j = 0;
-        for (T& i : *this) {
-            if (func(i)) {
-                res.push_back(i);
-            }
-        }
-        return res;
-    };
-
-    template<typename F>
-    T reduce(F func, T start) const {
-        T ans = start;
-        for (T& i : *this) {
-            ans = func(i, ans);
-        }
-        return ans;
-    };
+template<typename Container>
+concept vectorizable = requires (Container c, size_t idx) {
+    {c[idx]};//-> c::value_type;
+    {c.GetLenght()} -> std::convertible_to<size_t>;
+    {c.begin()};
+    {c.end()};
 
 };
 
-template<typename T>
-std::ostream& operator<<(std::ostream& os, const Vector<T>& v) {
-    os << "[";
-    for (auto i = v.begin(); i != (v.end() - 1); ++i) {
-        os << *i << ", ";
+template <template <typename> class Container, number T>
+requires vectorizable<Container<T>>
+class Vec {
+private:
+    Container<T> data_;
+public:
+    Vec() = delete;
+    explicit Vec(const Container<T>& c) : data_(c) {};
+    Vec(size_t size) : data_(size){};
+
+    Vec operator+(const Vec& other) const {
+        if (data_.GetLenght() != other.data_.GetLenght()) {
+            throw SizeMismatchException(data_.GetLenght(), other.data_.GetLenght());
+        }
+        auto pairs = zip(data_, other.data_);
+        auto sums = map(pairs, [](std::pair<T,T> p) {
+            return p.first + p.second;
+        });
+        return Vec(sums);
     }
-    os << *(v.end() - 1);
-    os << "]";
-    return os;
+    Vec operator-(const Vec& other) const {
+        if (data_.GetLenght() != other.data_.GetLenght()) {
+            throw SizeMismatchException(data_.GetLenght(), other.data_.GetLenght());
+        }
+        auto pairs = zip(data_, other.data_);
+        auto sums = map(pairs, [](std::pair<T,T> p) {
+            return p.first + p.second;
+        });
+        return Vec(sums);
 }
+
+    size_t GetLenght() const {
+        return data_.GetLenght();
+    }
+
+    T& operator[](size_t index){
+        return data_[index];
+    };
+    
+    const T& operator[](size_t index) const {
+        return data_[index];
+    };
+
+    Vec operator*(const T& scalar) {
+        Vec new_vec(*this);
+        for(size_t i = 0; i < new_vec.GetLenght(); i++) {
+            new_vec[i]*= scalar;
+        };
+        return new_vec;
+    };
+
+    T dot(const Vec& other) {
+        
+    };
+
+    Container<std::pair<T,T>> zip(const Container<T>& a, const Container<T>& b) const {
+        if (a.GetLenght() != b.GetLenght()) {
+            throw SizeMismatchException(a.GetLenght(), b.GetLenght());
+        }
+
+        Container<std::pair<T,T>> result;
+        for (size_t i = 0; i < a.GetLenght(); ++i) {
+            result.Append({a.Get(i), b.Get(i)});
+        }
+        return result;
+    };
+
+    template<typename F>
+    auto map(const Container<std::pair<T,T>>& seq, F func) const {
+        Container<T> result;
+        for (size_t i = 0; i < seq.GetLenght(); ++i) {
+            result.Append(func(seq.Get(i)));
+        }
+        return result;
+    }
+
+};
+
+
--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <span>
-#include "Structur/FuncList.h"
+#include "submodule/Sequence/ArraySequence.h"
 #include "Structur/Vector.h"
 
 int main() {
     int data[] = {1, 2, 3};
-    int data1[] = {1, 2};
-    Vector<int> sp(std::span<int>(data, 3));
-    Vector<int> sp1(std::span<int>(data1, 2));
-    std::cout << sp.dot(sp1);
+    ArraySequence<int> ase(std::span(data, 3));
+    Vec<ArraySequence, int> sp(ase);
+    Vec<ArraySequence, int> qqq = sp + sp;
+    for(size_t i = 0; i < qqq.GetLenght(); i++){
+        std::cout << qqq[i] << " ";
+    }
     return 0;
 }
