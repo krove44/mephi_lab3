41bd40b08804fdf3b292f99da39472c2f1ba789e
krove44 <krove8@gmail.com>
2026-05-17 21:13:19 +0300

    add drawText

--- new_main.cpp
+++ new_main.cpp
@@ -9,6 +9,17 @@ int main()
     );
     window.setFramerateLimit(60);
 
+    sf::Font font;
+    auto check = font.openFromFile("C:/Windows/Fonts/segoeui.ttf");
+
+
+    auto drawText = [&](const std::string& str, float x, float y, unsigned size = 14, sf::Color color = sf::Color{220, 220, 225}) {
+        sf::Text t(font, str, size);
+        t.setFillColor(color);
+        t.setPosition({x, y});
+        window.draw(t);
+    };
+
     while (window.isOpen())
     {
         while (const auto ev = window.pollEvent())
@@ -16,8 +27,12 @@ int main()
             if (ev->is<sf::Event::Closed>())
                 window.close();
         }
-
         window.clear(sf::Color{24, 24, 28});
+
+        drawText("Trajectory Gui", 20.f, 20.f, 24);
+        drawText("Parameters",            20.f, 60.f, 20, sf::Color{130, 130, 145});
+        drawText("v0 = 45 m/s",           20.f, 90.f, 16);
+
         window.display();
     }
 
